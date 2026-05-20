0f646d79881333bae62aa8ac5f7cbf7a3a075782
2CM <jahnfamilypdx@gmail.com>
2026-05-20 16:57:37 -0700

    revert contribution of vertical retention to conserved speed (maybe another time...)

--- Source/Extensions/PlayerExtensions.cs
+++ Source/Extensions/PlayerExtensions.cs
@@ -110,9 +110,6 @@ namespace Celeste.Mod.GooberHelper.Extensions {
                 ),
                 Utils.SignedAbsMax(
                     self.Speed.Y,
-                    ext.VerticalRetentionTimer > 0f
-                        ? ext.VerticalRetentionSpeed
-                        : 0f,
                     conserveBeforeDashSpeed
                         ? self.beforeDashSpeed.Y
                         : 0f,
